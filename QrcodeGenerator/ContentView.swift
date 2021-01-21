283ac15536cf61a0994464ccdc5ccf7ce9ba6f90
Elechim01 <mikymanny015@gmail.com>
2021-01-21 22:31:23 +0100

    Initial Commit

---
+++ QrcodeGenerator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  QrcodeGenerator
+//
+//  Created by Michele on 21/01/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ QrcodeGenerator/QrcodeGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  QrcodeGeneratorApp.swift
+//  QrcodeGenerator
+//
+//  Created by Michele on 21/01/21.
+//
+
+import SwiftUI
+
+@main
+struct QrcodeGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
